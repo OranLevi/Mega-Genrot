4e9c70a4a13aafbe2076b701298e263cd18ddf61
Oran Levi <100801409+OranLevi@users.noreply.github.com>
2022-11-05 15:02:25 +0200

    ListTableViewCell upload again to GitHub

---
+++ megaGenerator/ListTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ListableViewCell.swift
+//  MegaGenerator
+//
+//  Created by Oran Levi on 30/10/2022.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var listTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
